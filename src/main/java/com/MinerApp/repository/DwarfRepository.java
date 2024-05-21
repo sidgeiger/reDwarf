d0398c81086a907f9228f4284699a243be5e826b
zolHet <zoltan.hetenyi81@gmail.com>
2024-05-21 23:01:22 +0200

    DwarfRepository updated with Optional method

--- src/main/java/com/MinerApp/repository/DwarfRepository.java
+++ src/main/java/com/MinerApp/repository/DwarfRepository.java
@@ -4,6 +4,9 @@ import com.MinerApp.domain.Dwarf;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DwarfRepository extends JpaRepository<Dwarf, Long> {
+    Optional<Dwarf> findDwarfByName(String dwarfName);
 }
