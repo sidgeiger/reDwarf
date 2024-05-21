9248f4e1da588e7bfae581656881e5da232830b2
zolHet <zoltan.hetenyi81@gmail.com>
2024-05-21 23:00:48 +0200

    Custom ecxeptions created

---
+++ src/main/java/com/MinerApp/exceptions/ValidationError.java
@@ -0,0 +1,15 @@
+package com.MinerApp.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ValidationError {
+
+    private String field;
+
+    private String errorMessage;
+}
