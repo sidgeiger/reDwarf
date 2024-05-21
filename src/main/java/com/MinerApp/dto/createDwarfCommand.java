4c4cf6183fcf065a02b9ee425448c9da137aeb26
zolHet <zoltan.hetenyi81@gmail.com>
2024-05-21 22:10:02 +0200

    Dwarf related dto classes added

---
+++ src/main/java/com/MinerApp/dto/createDwarfCommand.java
@@ -0,0 +1,19 @@
+package com.MinerApp.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class createDwarfCommand {
+    @NotNull(message = "This field cannot be null.")
+    @NotEmpty(message = "This field cannot be empty.")
+    @NotBlank(message = "This field cannot be blank.")
+    private String name;
+
+    @NotNull(message = "This field cannot be null.")
+    @Min(1)
+    private int productivity;
+}
