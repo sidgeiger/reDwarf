a1e7367c93d62a97d8d15dbe80d41bf1a123267b
zolHet <zoltan.hetenyi81@gmail.com>
2024-06-01 21:55:31 +0200

    created dto classes for items

---
+++ src/main/java/com/MinerApp/dto/ItemInfo.java
@@ -0,0 +1,18 @@
+package com.MinerApp.dto;
+
+import lombok.Data;
+
+@Data
+public class ItemInfo {
+
+    private Long id;
+
+    private String name;
+
+    private int bonus;
+
+    private int value;
+
+    private String dwarfName;
+
+}
