3b8562f6cf86fa7fe916c075aa8f0ef3f38f1993
zolHet <zoltan.hetenyi81@gmail.com>
2024-07-03 21:06:58 +0200

    runeListIterator(), runeSave() added to copy new runes to Rune Table

--- src/main/java/com/MinerApp/service/RuneService.java
+++ src/main/java/com/MinerApp/service/RuneService.java
@@ -10,8 +10,12 @@ import com.MinerApp.repository.RuneRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -25,7 +29,7 @@ public class RuneService {
     private ModelMapper modelMapper;
 
     @Autowired
-    public RuneService(RuneRepository runeRepository, ItemService itemService, ModelMapper modelMapper) {
+    public RuneService(RuneRepository runeRepository, @Lazy ItemService itemService, ModelMapper modelMapper) {
         this.runeRepository = runeRepository;
         this.itemService = itemService;
         this.modelMapper = modelMapper;
@@ -41,5 +45,28 @@ public class RuneService {
         runeInfo.setItemId(item.getId());
         return runeInfo;
 
+    }
+
+    public List<Rune> runeListIterator(List<Rune> runeList) {
+        List<Rune> newRuneListForSettingItemId = null;
+        if (!runeList.isEmpty()) {
+            newRuneListForSettingItemId = new ArrayList<>();
+            for (Rune rune : runeList) {
+                Rune newRune = new Rune();
+                newRune.setBanned(rune.isBanned());
+                newRune.setBonus(rune.getBonus());
+                newRune.setName(rune.getName());
+                newRune.setItem(rune.getItem());
+                Rune savedRune = this.runeSave(newRune);
+                newRuneListForSettingItemId.add(savedRune);
+            }
+
+        }
+        return newRuneListForSettingItemId;
+    }
+
+    private Rune runeSave(Rune rune) {
+        runeRepository.save(rune);
+        return rune;
     }
 }
