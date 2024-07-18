871c128bedb403b77ad71423f5b965e7d0f766cc
zolHet <zoltan.hetenyi81@gmail.com>
2024-07-18 22:42:42 +0200

    JPQL query started to be implemented...

--- src/main/java/com/MinerApp/repository/DwarfRepository.java
+++ src/main/java/com/MinerApp/repository/DwarfRepository.java
@@ -1,7 +1,9 @@
 package com.MinerApp.repository;
 
 import com.MinerApp.domain.Dwarf;
+import com.MinerApp.dto.BestDwarvesNames;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +11,8 @@ import java.util.Optional;
 @Repository
 public interface DwarfRepository extends JpaRepository<Dwarf, Long> {
     Optional<Dwarf> findDwarfByName(String dwarfName);
+
+    //JPQL query to be implemented
+    @Query("SELECT new com.MinerApp.dto.BestDwarvesNames()")
+    BestDwarvesNames getBestDwarves();
 }
