abb3223b48135b237d56f3134fff9b61ad706ed3
zolHet <zoltan.hetenyi81@gmail.com>
2024-07-24 23:23:02 +0200

    main logic with JQPL implemented

--- src/main/java/com/MinerApp/repository/RuneRepository.java
+++ src/main/java/com/MinerApp/repository/RuneRepository.java
@@ -1,8 +1,11 @@
 package com.MinerApp.repository;
 
 import com.MinerApp.domain.Rune;
+import com.MinerApp.dto.RunesWithAvgBonusesInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RuneRepository extends JpaRepository<Rune, Long> {
@@ -11,4 +14,6 @@ public interface RuneRepository extends JpaRepository<Rune, Long> {
     Optional<Rune> findRuneByBonus(int runeBonus);
     Optional<Rune> findRuneByBanned(boolean banned);
 
+    @Query("SELECT DISTINCT new com.MinerApp.dto.RunesWithAvgBonusesInfo (r.name, avg(r.bonus)) FROM Rune as r WHERE r.banned = false AND r.item IS NOT null GROUP BY r.name ORDER BY r.name DESC")
+    List<RunesWithAvgBonusesInfo> runesWithAvgBonusesDistinctNamesJQPL();
 }
