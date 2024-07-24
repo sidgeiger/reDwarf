bce4c1ec05623691eb5753a50841681961caada2
zolHet <zoltan.hetenyi81@gmail.com>
2024-07-24 22:26:32 +0200

    new dto added

---
+++ src/main/java/com/MinerApp/dto/RunesWithAvgBonuesInfo.java
@@ -0,0 +1,14 @@
+package com.MinerApp.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class RunesWithAvgBonuesInfo {
+
+    private List<String> names;
+
+    private List<Integer> bonuses;
+
+}
